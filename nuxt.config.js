f431ec8e8c5e67ad92f60e6d76271aa0d6a0b3fa
Lenine Djouatsa <Lenine.Djouatsa@arrow.com>
2020-11-19 07:15:55 +0100

    Initial template

---
+++ nuxt.config.js
@@ -0,0 +1,74 @@
+export default {
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'njigui',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/assets/images/favicons/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/assets/images/favicons/favicon-16x16.png' },
+
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Poppins:300,400,500,500i,600,700,800%7CSatisfy&display=swap" },
+
+      { rel: "stylesheet", href: "/assets/css/animate.min.css" },
+      { rel: "stylesheet", href: "/assets/css/bootstrap.min.css" },
+      { rel: "stylesheet", href: "/assets/css/owl.carousel.min.css" },
+      { rel: "stylesheet", href: "/assets/css/owl.theme.default.min.css" },
+      { rel: "stylesheet", href: "/assets/plugins/fontawesome-free-5.11.2-web/css/all.min.css" },
+      { rel: "stylesheet", href: "/assets/plugins/kipso-icons/style.css" },
+      { rel: "stylesheet", href: "/assets/css/magnific-popup.css" },
+      { rel: "stylesheet", href: "/assets/css/vegas.min.css" },
+      { rel: "stylesheet", href: "/assets/css/style.css" },
+      { rel: "stylesheet", href: "/assets/css/responsive.css" },
+
+    ],
+
+    script: [
+      { src: "/assets/js/jquery.min.js", body: true },
+      { src: "/assets/js/bootstrap.bundle.min.js", body: true },
+      { src: "/assets/js/waypoints.min.js", body: true },
+      { src: "/assets/js/owl.carousel.min.js", body: true },
+      { src: "/assets/js/jquery.counterup.min.js", body: true },
+      { src: "/assets/js/TweenMax.min.js", body: true },
+      { src: "/assets/js/wow.js", body: true },
+      { src: "/assets/js/jquery.magnific-popup.min.js", body: true },
+      { src: "/assets/js/countdown.min.js", body: true },
+      { src: "/assets/js/vegas.min.js", body: true },
+      { src: "/assets/js/jquery.validate.min.js", body: true },
+      { src: "/assets/js/jquery.ajaxchimp.min.js", body: true },
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
